2e7534839cf690ef7516e1655bc7337ca2e908ea
Jakub <Jakub@DESKTOP-V1K84ML>
2016-12-07 17:33:36 +0100

    Added FANN c# wrapper

--- SNP2/SNP2/Program.cs
+++ SNP2/SNP2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FANNCSharp;
+using FANNCSharp.Float;
 
 namespace SNP2
 {
@@ -10,6 +12,13 @@ namespace SNP2
     {
         static void Main(string[] args)
         {
+            uint[] layers = { 2, 3, 1 };
+            NeuralNet net = new NeuralNet(NetworkType.LAYER, layers);
+            
+            net.RandomizeWeights(0, 1);
+            net.TrainOnFile("data.dat", 100, 1, (float)0.0001);
+            net.Save("NN.net");
+            Console.ReadLine();
         }
     }
 }
