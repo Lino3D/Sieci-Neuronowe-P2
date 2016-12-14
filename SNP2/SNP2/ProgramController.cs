ac21e14acdb4c4203904fe65bb29ce853f92cafa
Lino3D <kamiskijakub@gmail.com>
2016-12-14 16:35:57 +0100

    Zmiany zmiennych lokalnych na małą literę.

--- SNP2/SNP2/ProgramController.cs
+++ SNP2/SNP2/ProgramController.cs
@@ -22,6 +22,8 @@ namespace SNP2
             uint[] layers = { 2, 3, 1 };
             NeuralNet net = new NeuralNet(NetworkType.LAYER, layers);
 
+            
+
             net.RandomizeWeights(0, 1);
             net.TrainOnFile("data.dat", 1000, 1, (float)0.0001);
             net.Save("NN.net");
