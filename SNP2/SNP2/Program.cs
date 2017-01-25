25d991026ea8fbaae1d33b457f38ec5bb8bb9f28
grabkipower <michcio91@gmail.com>
2017-01-26 00:18:51 +0100

    c

--- SNP2/SNP2/Program.cs
+++ SNP2/SNP2/Program.cs
@@ -30,7 +30,7 @@ namespace SNP2
 
             //var files = Directory.GetFiles("./Resource/");
 
-            InitializeDocumentWithoutSerialization
+            InitializeDocumentWithoutSerialization();
             //controller.InitializeDocument();
             //controller.CreateNodes();
             controller.InitializeNNForDocuments();
